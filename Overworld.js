2437fbc71a3846c20e802e9ddb3b61f10c8f15f5
emre-f <97183433+emre-f@users.noreply.github.com>
2022-05-18 14:47:22 -0400

    chararcter rendered

--- Overworld.js
+++ Overworld.js
@@ -11,5 +11,40 @@ class Overworld {
             this.ctx.drawImage(image, 0, 0); // which image to get pixels from and x, y coordinates
         };
         image.src = "images/maps/DemoLower.png" // assign source to image
+
+        const x = 5;
+        const y = 6;
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow, // image we are drawing from
+                0, // starting point of left crop
+                0, // the top cut
+                32, // pixel width of cut
+                32, // pixel height of cut
+                x * 16 - 8, // x-coord of where we are drawing [x16 because each grid is 16x16 pixels]
+                y * 16 + 18, // y-coord of where we are drawing
+                32, // the actual scale we'll draw in (can be used to scale up/down) [width]
+                32 // [height]
+            );
+        };
+        shadow.src = "images/characters/shadow.png"
+
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero, // image we are drawing from
+                0, // starting point of left crop
+                0, // the top cut
+                32, // pixel width of cut
+                32, // pixel height of cut
+                x * 16 - 8, // x-coord of where we are drawing [x16 because each grid is 16x16 pixels]
+                y * 16 + 18, // y-coord of where we are drawing
+                32, // the actual scale we'll draw in (can be used to scale up/down) [width]
+                32 // [height]
+            );
+        };
+        hero.src = "images/characters/people/hero.png"
     };
 }
